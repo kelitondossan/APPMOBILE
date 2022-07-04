bebbb9768e1ad59dee9b81d1989c50cb56b53fed
keliton santos <71714438+kelitondossan@users.noreply.github.com>
2022-07-04 17:48:39 -0300

    ARQUIVOS

---
+++ App6/App6/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using App6.ViewModels;
+using App6.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
